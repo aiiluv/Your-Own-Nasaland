de91d0b9e3940c1fbef6a0abbc85474e8df75228
aiiluv <airinazaria.h.putri@gmail.com>
2026-09-24 12:57:57 +0700

    add style

--- src/main.js
+++ src/main.js
@@ -4,16 +4,22 @@ document.querySelector("#app").innerHTML = "<p>loading...</p>";
 
 // Gunakan BACKTICK (``) untuk string interpolasi ${API_KEY}
 fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
-  // Wajib konversi response ke JSON lebih dulu
-  .then((res) => res.json())
-  .then((data) => {
+  .then(response => response.json())
+  .then(data => {
+    let media;
+
+    if (data.media_type === "image") {
+      media = `<img src="${data.url}"/>`;
+    } else {
+      media = `<video src="${data.url}" controls></video>`;
+    }
+
     document.querySelector("#app").innerHTML = `
-        <h1>${data.title}</h1>
-        <img src="${data.url}" alt="${data.title}" />
-        <p>${data.explanation}</p>
+      <h1>${data.title}</h1>
+      ${media}
+      <p>${data.explanation}</p>
     `;
   })
-  .catch((err) => {
-    console.error(err);
-    document.querySelector("#app").innerHTML = "<p>Gagal memuat data.</p>";
+  .catch(err => {
+    document.querySelector("#app").innerHTML = `<p>Error: ${err.message}</p>`;
   });
